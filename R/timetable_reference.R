03f0613b6e9f3779d496e5e58e2a4e00ec3fa30c
ottlngr <philipp@ottolinger.de>
2016-12-29 14:18:37 +0100

    more documentation

--- R/timetable_reference.R
+++ R/timetable_reference.R
@@ -1,4 +1,23 @@
+#' @name timetable_reference
+#' @title Get detailed information on a specific train
+#' @description Use a reference URL from either a departure or an arrival board to get more detailed information on a train.
+#' @export timetable_station
+#'
+#' @param reference character string. A reference URL from \code{timetable_arrivals()} or \code{timetable_departures()}.
+#'
+#' @return A \code{list} with journey details.
+#'
+#' @author Philipp Ottolinger
+#' @references \url{http://data.deutschebahn.com/dataset/api-fahrplan}
+#' @seealso \code{\link{timetable_departures}}, \code{\link{timetable_arrivals}}, \code{\link{timetable_station}}
+#'
 #' @importFrom jsonlite fromJSON
+#'
+#' @examples
+#' \dontrun{
+#' tt <- timetable_arrivals(station = berlin, direction = hamburg, date = "2016-12-31", time = "12:00", authkey = authKey, reference = TRUE)
+#' timetable_reference(as.character(tt$ref[1]))
+#' }
 
 timetable_reference <- function(reference) {
   api_result <- jsonlite::fromJSON(reference)
