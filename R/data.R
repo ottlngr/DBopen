5adb54b8a6f9f4eab000c43e70f7188601051ff0
ottlngr <philipp@ottolinger.de>
2016-12-29 12:17:40 +0100

    more documentation

--- R/data.R
+++ R/data.R
@@ -14,3 +14,24 @@
 #' @references \url{http://data.deutschebahn.com/dataset/data-haltestellen}
 #' @source \url{http://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2016_01_alle.csv}
 "haltestellendaten"
+
+#' @name stationsdaten
+#' @title List of all stations with addresses.
+#'
+#' @format A \code{data.frame} with 5372 observations and 10 variables:
+#' \describe{
+#'   \item{Bundesland}{The respective federal state.},
+#'   \item{BM}{The respective station management.}
+#'   \item{Bf..Nr.}{Unique station number.}
+#'   \item{Station}{Name of the station.},
+#'   \item{Bf.DS.100.Abk.}{Corresponding railway system.}
+#'   \item{Kat..Vst}{Station category.},
+#'   \item{Straße}{Street, as part of address.}
+#'   \item{PLZ}{Postcode, as part of address.},
+#'   \item{Ort}{City, as part of address.}
+#'   \item{Aufgabenträger}{Constractor.}
+#' }
+#'
+#' @references \url{http://data.deutschebahn.com/dataset/data-stationsdaten}
+#' @source \url{http://download-data.deutschebahn.com/static/datasets/stationsdaten/DBSuS-Uebersicht_Bahnhoefe-Stand2016-07.csv}
+"stationsdaten"
